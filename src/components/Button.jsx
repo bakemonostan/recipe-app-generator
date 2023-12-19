5f6ae87ce8ebd342dd7f46c7c0e89216a013a475
bakemonostan <ehizomokhomion@gmail.com>
2023-12-19 20:54:21 +0100

    ...

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
 export default function Button({ onClick, label }) {
-    return (
-        <button
-            className='bg-primary text-white font-bold w-[101px] h-9 rounded-[10px] shadow'
-            onClick={onClick}
-        >
-            {label}
-        </button>
-    );
+  return (
+    <button
+      className="bg-primary text-white font-bold w-[101px] h-9 rounded-[10px] shadow"
+      onClick={onClick}
+      id="gtm-btn-test"
+    >
+      {label}
+    </button>
+  );
 }
